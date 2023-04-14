d5bbc00d63b614b4458ef36acbf449e87aac24d6
Marcondes Ferreira <bshookss@gmail.com>
2023-04-13 23:31:50 -0300

    ModalDelet finished, initil the logic de reduxHook

---
+++ src/shared/redux/user/actions.ts
@@ -0,0 +1,11 @@
+import { UserActionTypes } from "./action-types"
+
+export const loginUser = (payload: any) => ({
+    type : UserActionTypes.LOGIN,
+    payload,
+  
+});
+
+export const logoutUser = () => ({
+  type: UserActionTypes.LOGOUT
+})
